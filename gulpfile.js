4822177a75f057dcf18b31736c01aae2776f2cb6
annahrynykha <anna.grynyha@gmail.com>
2024-09-26 01:14:36 +0300

    Tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,62 +1,3 @@
-/*const { src, dest, series, parallel, watch } = require('gulp');
-const file_include = require('gulp-file-include');
-const concat = require('gulp-concat');
-const sass = require('gulp-sass')(require('sass'));
-const postcss = require('gulp-postcss');
-const cssnano = require('cssnano');
-const rename = require('gulp-rename');
-const uglify = require('gulp-uglify');
-const imagemin = require('gulp-imagemin');
-
-const gulp = require('gulp');
-
-//html
-const html_task = () =>src('app/*.html')
-    .pipe(dest('dist'));
-
-//scss
-const scss_task = () => src('app/scss/*.scss')
-    .pipe(concat('index.scss'))
-    .pipe(sass().on('error', sass.logError))
-    .pipe(cssnano())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(dest('dist/css'));
-
-//js
-const js_task = () => src('app/js/*.js')
-    .pipe(concat('index.js'))
-    .pipe(uglify())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(dest('dist/js'));
-
-//img
-const img_task = () => src('app/img/*.+(jpg|jpeg|png|gif)')
-    .pipe(imagemin({
-        progressive: true,
-        svgoPlugins: [{removeViewBox: false}],
-        interLaced: true
-    }))
-    .pipe(dest('dist/img'));
-
-//browserSync
-const browserSync_task = () =>browserSync.init({
-    server: {
-        baseDir: 'dist/',
-    }
-});
-
-//watch
-const watch_task = () => {
-    browserSync_task();
-    watch('app/*.html', parallel(html_task));
-    watch('app/scss/*.scss', parallel(scss_task));
-    watch('app/js/*.js', parallel(js_task));
-    watch('app/img/*.+(jpg|jpeg|png|gif)', img_task);
-}
-
-gulp.task('default', gulp.parallel('browser-sync','watch'));
-*/
-
 const {src, dest} = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
@@ -110,7 +51,7 @@ gulp.task('watch', () => {
     gulp.watch('app/js/*.js', gulp.series('uglify'));
     gulp.watch('app/index.html', gulp.series('html'));
     gulp.watch('app/html/*.html', gulp.series('html'));
-    //gulp.watch('app/img/*.png', gulp.series('img'));
+    gulp.watch('app/img/*', gulp.series('img'));
 });
 
 // Update browser
@@ -124,3 +65,4 @@ gulp.task('browser-sync', () => {
 });
 
 gulp.task('default',gulp.parallel('browser-sync','watch'));
+/*gulp.task('default', gulp.series('html', 'sass', 'uglify','img', gulp.parallel('browser-sync','watch')));*/
